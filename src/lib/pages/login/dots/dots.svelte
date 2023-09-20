5a76f37ea231f39891835e8fa6b2554857222c35
kursten putman <fglad@gmail.com>
2023-09-20 11:22:47 -0500

    add login, footer, and dots

---
+++ src/lib/pages/login/dots/dots.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Dot from "./dot.svelte";
+
+</script>
+<div class=" absolute  w-screen h-screen blur-lg">
+  <Dot></Dot>
+  <Dot></Dot>
+  <Dot></Dot>
+  <Dot></Dot>
+  
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
     import Title from "$lib/pages/login/title.svelte";
+    import Login from "$lib/pages/login/login.svelte";
+    import Footer from "$lib/pages/login/footer.svelte";
+    import Dots from "$lib/pages/login/dots/dots.svelte";
 </script>
 
 <div class="bg-gradient-to-b from-backgroundSecondary to-background w-screen h-screen">
     <Title />
+    <Login />
+    <Footer />
+    <Dots/>
 </div>
