4fd0cbedbf2a6ea3f991b900b902ec39e12c3b8f
kursten putman <fglad@gmail.com>
2023-09-21 11:29:47 -0500

    app scree, and finishing dots

--- src/lib/pages/login/dots/dot.svelte
+++ src/lib/pages/login/dots/dot.svelte
@@ -1,1 +1,12 @@
-<div class="absolute w-[30px] aspect-square rounded-full  bg-brand-600"/>
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let dot: HTMLDivElement;
+
+    onMount(() => { 
+        dot.style.left = `${Math.random() * window.innerWidth}px`;
+        dot.style.top = `${Math.random() * window.innerHeight}px`;
+    });
+</script>
+
+<div class="absolute w-[30px] aspect-square rounded-full bg-brand-600" bind:this={dot} />
---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="absolute top-100px w-55% left-20 text-emerald-300 text-4xl">home</h1>
+<h2 class="absolute top-[15%] w-55% left-14 bg-brand rounded-xl px-12 py-4 text-2xl text-black ">place order</h2>
+<h2 class="absolute top-1/4 w-55% left-16 text-emerald-300 text-xl">Restuarants</h2>
+<h3 Class="absolute top-1/3 left-20 w-55% text-amber-300 bg-red-600 rounded-xl px-3  h-6 text-lg">McDonalds</h3>
