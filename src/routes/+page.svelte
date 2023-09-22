01ab2fd31b311caa3a532ab74fb2827d088dd7a7
kursten putman <fglad@gmail.com>
2023-09-22 11:27:43 -0500

    home page design work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="text-black">demo sign in</h1>
-<a href="/auth/signin" class="text-black" data-sveltekit-preload-data="off">Sign in</a>
+<h1> demo sign in</h1>
+<a href="/auth/signin" data-sveltekit-preload-data="off">Sign in</a>
