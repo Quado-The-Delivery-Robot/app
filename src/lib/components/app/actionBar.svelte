6882a046624b388997a601631919aec1f5bb0364
Landon Redmond <name1234561.po@gmail.com>
2023-09-27 10:17:55 -0500

    Start work on action bar.

---
+++ src/lib/components/app/actionBar.svelte
@@ -0,0 +1,5 @@
+<div class="w-screen flex justify-center items-center gap-4 children:aspect-square py-3 children:h-6 descendant:w-full descendant:h-full">
+    <a href="/settings">
+        <img src="/images/settings.svg" alt="settings" />
+    </a>
+</div>
---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import ActionBar from "$lib/components/app/actionBar.svelte";
+</script>
+
+<div class="flex justify-center items-center flex-col">
+    <div class="flex-grow w-screen overflow-y-auto">
+        <slot />
+    </div>
+
+    <ActionBar />
+</div>
