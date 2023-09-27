7fda9316372effe82ea4cf6f9ed85e30625d1a41
Landon Redmond <name1234561.po@gmail.com>
2023-09-27 11:29:55 -0500

    Worked on action bar design.

--- src/lib/components/app/actionBar.svelte
+++ src/lib/components/app/actionBar.svelte
@@ -1,5 +1,38 @@
-<div class="w-screen flex justify-center items-center gap-4 children:aspect-square py-3 children:h-6 descendant:w-full descendant:h-full">
-    <a href="/settings">
-        <img src="/images/settings.svg" alt="settings" />
-    </a>
+<script lang="ts">
+    type button = {
+        name: string;
+        url: string;
+        image: string;
+    };
+
+    const BUTTONS: button[] = [
+        {
+            name: "Home",
+            url: "/",
+            image: "/images/home/outline.svg",
+        },
+        {
+            name: "Orders",
+            url: "/orders",
+            image: "/images/shoppingBasket/outline.svg",
+        },
+        {
+            name: "Settings",
+            url: "/settings",
+            image: "/images/settings/outline.svg",
+        },
+        {
+            name: "Settings",
+            url: "/settings",
+            image: "/images/settings/outline.svg",
+        },
+    ];
+</script>
+
+<div class="absolute bottom-0 w-screen h-auto flex justify-between items-center pt-3.5 pb-5 px-9 z-10 bg-background" id="actionBar">
+    {#each BUTTONS as button}
+        <a href={button.url}>
+            <img src={button.image} alt={button.name} class="w-full aspect-square h-7" />
+        </a>
+    {/each}
 </div>
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -2,6 +2,6 @@
     import Section from "$lib/pages/app/home/section.svelte";
 </script>
 
-<h1 class="text-6xl mb-4 p-2 mt-5 ">Home</h1>
+<h1 class="text-6xl mb-4 p-2 mt-5">Home</h1>
 
 <Section endpoint="/api/restaurants/recommended" sectionName="Recommendations" />
