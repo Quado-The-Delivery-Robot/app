4bd25c871af981de85979714cc624340546ad490
kursten putman <fglad@gmail.com>
2023-09-29 11:30:07 -0500

    Worked on orders page design.

--- src/lib/pages/app/orders/orders.svelte
+++ src/lib/pages/app/orders/orders.svelte
@@ -1,4 +1,15 @@
-<h1 class=" flex-col top-12 left-8 text-brand text-4xl">Order</h1>
-<div class=" rounded-md bg-backgroundSecondary px-4 py-4">
-    <p class="text-lg text-left">Subway<p />
+<h1 class=" flex-col top-12 left-8 text-brand text-4xl m-4">Order History</h1>
+<div class=" rounded-xl bg-backgroundSecondary px-4 py-4 ">
+    <p class="text-xl text-left m-2">
+        Subway
+        <span class="  ml-2 text-sm text-primary-700"> 1 quanity - Italian 6" - $6.99</span>
+        <span class=" text-sm text-primary-700">-Delivered- 9/29/23</span>
+    </p>
+
+    <p class="text-xl text-left m-2">
+        Burger King
+        <span class=" ml-2 text-sm text-primary-700"> 1 quanity - Whopper - $4.19</span>
+        <span class="text-sm text-primary-700">-Delivered - 9/27/23</span>
+    </p>
+
 </div>
