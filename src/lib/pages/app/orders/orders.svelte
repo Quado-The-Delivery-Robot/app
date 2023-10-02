46a24399309eba981608fe893ec848062a8cd350
kursten putman <fglad@gmail.com>
2023-10-02 11:19:29 -0500

    worked on order page and in progress order box

--- src/lib/pages/app/orders/orders.svelte
+++ src/lib/pages/app/orders/orders.svelte
@@ -1,15 +1,30 @@
-<h1 class=" flex-col top-12 left-8 text-brand text-4xl m-4">Order History</h1>
-<div class=" rounded-xl bg-backgroundSecondary px-4 py-4 ">
-    <p class="text-xl text-left m-2">
-        Subway
-        <span class="  ml-2 text-sm text-primary-700"> 1 quanity - Italian 6" - $6.99</span>
-        <span class=" text-sm text-primary-700">-Delivered- 9/29/23</span>
-    </p>
-
-    <p class="text-xl text-left m-2">
-        Burger King
-        <span class=" ml-2 text-sm text-primary-700"> 1 quanity - Whopper - $4.19</span>
-        <span class="text-sm text-primary-700">-Delivered - 9/27/23</span>
-    </p>
-
+<h1>Order History</h1>
+<div class=" flex-col flex gap-4">
+    <div class=" inProgress rounded-xl  px-4 py-4 border border-backgroundSecondary">
+        <p class="text-xl text-left m-2 font-medium">Subway</p>
+        <p class=" text-sm text-slate-100 font-normal">In progress</p>
+        <p class="  ml-2 text-sm text-primary-700 font-normal">1 quanity - Italian 6" - $6.99</p>
+    </div>
+    <div class=" rounded-xl bg-backgroundSecondary px-4 py-4">
+        <p class=" text-sm text-primary-700 font-normal">Delivered - 9/29/23</p>
+        <p class="text-xl text-left m-2 font-medium">
+            Subway
+            <span class="  ml-2 text-sm text-primary-700 font-normal"> 1 quanity - Italian 6" - $6.99</span>
+        </p>
+    </div>
+    <div class=" rounded-xl bg-backgroundSecondary px-4 py-4">
+        <p class=" text-sm text-primary-700 font-normal">Delivered - 9/29/23</p>
+        <p class="text-xl text-left m-2 font-medium">
+            Subway
+            <span class="  ml-2 text-sm text-primary-700 font-normal"> 1 quanity - Italian 6" - $6.99</span>
+        </p>
+    </div>
 </div>
+
+<style>
+    .inProgress {
+        -webkit-box-shadow: 0px 0px 52px -3px rgba(70, 235, 165, 0.2);
+        -moz-box-shadow: 0px 0px 52px -3px rgba(70, 235, 165, 0.2);
+        box-shadow: 0px 0px 52px -3px rgba(70, 235, 165, 0.2);
+    }
+</style>
