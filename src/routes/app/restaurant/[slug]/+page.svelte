5948979532976ce09d3697683203c257e4961442
kursten putman <fglad@gmail.com>
2023-10-06 11:29:25 -0500

    worked on restaurant page details

---
+++ src/routes/app/restaurant/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import PageTitle from "$lib/components/pageTitle.svelte";
+    import { onMount } from "svelte";
+    onMount (()=>{
+        const element = document.getElementById("background")!;
+        element.style.backgroundColor="#002a06"
+    })
+</script>
+<PageTitle>Subway</PageTitle>
+
+
