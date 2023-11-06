c4f746cb34b58e732a94af1bdeab05470c704385
Landon Redmond <name1234561.po@gmail.com>
2023-11-06 10:20:30 -0600

    Silence unused dot variable.

--- src/lib/pages/login/dots/dots.svelte
+++ src/lib/pages/login/dots/dots.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="absolute w-screen h-screen blur-lg top-0 left-0 z-[0] overflow-hidden">
-    {#each Array(4) as dot}
+    {#each Array(4) as _dot}
         <Dot />
     {/each}
 </div>
