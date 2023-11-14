bb0d4ca70af309a9e4cd87109a92741149ef0a0e
kursten putman <fglad@gmail.com>
2023-11-14 11:29:46 -0600

    worked on cart page and container style

--- src/routes/app/cart/+page.svelte
+++ src/routes/app/cart/+page.svelte
@@ -4,6 +4,6 @@
 
 <PageTitle>Cart</PageTitle>
 
-<button>
-    
+<button class="flex " >
+    <div class="bg-red-500 border-2 border-red-500 h-full rounded-lg pd-4 text-left"> dummy</div>
 </button>
