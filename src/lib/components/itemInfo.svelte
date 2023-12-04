5f1a45654f882dcc769ca503e4054cf6df3183d6
Landon Redmond <name1234561.po@gmail.com>
2023-12-04 11:30:14 -0600

    Add dragging.

--- src/lib/components/itemInfo.svelte
+++ src/lib/components/itemInfo.svelte
@@ -4,6 +4,7 @@
     import { onMount } from "svelte";
 
     let container: HTMLDivElement;
+    let infoContainer: HTMLDivElement;
     let isDragging: boolean = false;
     $: data = {
         name: "",
@@ -19,7 +20,7 @@
         container.style.display = "block";
 
         anime({
-            targets: container,
+            targets: infoContainer,
             translateY: ["100%", "0%"],
             duration: 1000,
             easing: "easeOutQuint",
@@ -32,12 +33,22 @@
 
     function stopDragging() {
         isDragging = false;
+        infoContainer.style.top = "";
     }
 
-    function drag() {
-        if (isDragging) {
-            console.log("mouse moved");
+    function drag(event: MouseEvent | TouchEvent) {
+        if (!isDragging) return;
+
+        let yPosition: number = 0;
+
+        if (event.type == "touchmove") {
+            var touch = (event as any).originalEvent.touches[0] || (event as any).originalEvent.changedTouches[0];
+            yPosition = touch.pageY;
+        } else if (event.type == "mousemove") {
+            yPosition = (event as any).clientY;
         }
+
+        infoContainer.style.top = `${yPosition}px`;
     }
 
     onMount(() => {
@@ -46,13 +57,17 @@
     });
 </script>
 
-<div class="hidden absolute w-screen h-1/2 bottom-0 bg-backgroundSecondary z-20 left-0 rounded-t-[1.5rem] px-7 pt-6" bind:this={container} style="position: absolute;">
-    <button on:mousedown={startDragging} on:mouseup={stopDragging} on:touchstart={startDragging} on:touchend={stopDragging} class="w-full h-4 absolute left-0 top-0 bg-red-500" />
+<div class="hidden absolute top-0 left-0 w-screen h-screen overflow-hidden" bind:this={container}>
+    <div class="absolute w-screen h-1/2 bottom-0 bg-backgroundSecondary z-20 left-0 rounded-t-[1.5rem] px-7 pt-6" bind:this={infoContainer}>
+        <button on:mousedown={startDragging} on:mouseup={stopDragging} on:touchstart={startDragging} on:touchend={stopDragging} class="w-full h-4 absolute left-0 top-0 bg-red-500" />
 
-    <p class=" text-xl font-semibold font-poppins" style="color:aliceblue ">{data.name}</p>
-    <div class="flex justify-center gap-4 mb-4">
-        <p class=" text-base text-ellipsis text-left" style="color:aliceblue">${data.price}</p>
-        <p class=" text-base text-ellipsis text-left" style="color:aliceblue">{data.calories} calories</p>
+        <p class="text-2xl font-semibold font-poppins">{data.name}</p>
+
+        <div class="flex justify-center gap-4 mb-4">
+            <p class=" text-base text-ellipsis text-left">${data.price}</p>
+            <p class=" text-base text-ellipsis text-left">{data.calories} calories</p>
+        </div>
+
+        <p class="text-base text-ellipsis text-left">{data.description}</p>
     </div>
-    <p class=" text-base text-ellipsis text-left" style="color:aliceblue">{data.description}</p>
 </div>
